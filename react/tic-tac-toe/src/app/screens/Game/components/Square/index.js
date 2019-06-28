5ee077f38ecc8a9b3c7d556f7051ff0cdaa10d41
Mariana <mariana.calmon@wolox.com.ar>
2019-06-28 17:52:04 -0300

    Change of square component of function to class and add state.

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './styles.module.scss';
 
-const Square = ({value}) => (
-  <button type="button" className={styles.square} onClick={() => { alert('click'); }}>
-    {value}
-  </button>
-);
+
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button type="button" className={styles.square} onClick={() => { alert('click'); }}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
 
 export default Square;
