265bc0fd692e0502ab6f74c67d16707af6643236
Mariana <mariana.calmon@wolox.com.ar>
2019-06-28 17:29:06 -0300

    Add event handler on button.

--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 const Square = ({value}) => (
-  <button type="button" className={styles.square}>
+  <button type="button" className={styles.square} onClick={() => { alert('click'); }}>
     {value}
   </button>
 );
