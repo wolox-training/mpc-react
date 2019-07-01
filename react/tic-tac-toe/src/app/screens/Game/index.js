5c17c0494f4c0c30234773849ef34e31da323bae
Mariana <mariana.calmon@wolox.com.ar>
2019-07-01 17:31:24 -0300

    Change component game of function to class.

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import styles from './styles.module.scss';
 import Board from './components/Board';
 
-function Game () {
-  return (
-    <div className={styles.game}>
-      <div className={styles.gameBoard}>
-        <Board />
+class Game extends Component {
+  state = {
+    history: [{
+      square: Array(9).fill(null),
+    }],
+    xIsNext: true,
+  }
+  render() {
+    return (
+      <div className={styles.game}>
+        <div className={styles.gameBoard}>
+          <Board />
+        </div>
+        <div className={styles.gameInfo}>
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Game;
