95c9cdae1bcf17ba3079f1a1e2fa88579dd35636
Mariana <mariana.calmon@wolox.com.ar>
2019-07-02 16:59:28 -0300

    History of movements.

--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -10,6 +10,7 @@ class Game extends Component {
         squares: Array(9).fill(null)
       }
     ],
+    stepNumber: 0,
     xIsNext: true
   };
 
@@ -30,14 +31,31 @@ class Game extends Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
-    const { history } = this.state;
-    const current = history[history.length - 1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[this.state.stepNumber];
     const winner = current && calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move #  ${move}` : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -53,7 +71,7 @@ class Game extends Component {
         </div>
         <div className={styles.gameInfo}>
           {/* <div>{status}</div> */}
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
