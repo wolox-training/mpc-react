3ba2c90432d66eecd7e3c2946c4fc683492bed8b
Mariana <mariana.calmon@wolox.com.ar>
2019-07-02 14:35:05 -0300

    Installation and use of PropTypes.

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -1,67 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Square from '../Square';
 
 import styles from './styles.module.scss';
 
-class Board extends Component {
-  state = {
-    squares: Array(9).fill(null),
-    xIsNext: true
-  };
-
-  handleClick = (i) => {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
-  render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = `Winner:  ${winner}`;
-    } else {
-      status = `Next player:  ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-      <div>
-        <div className={styles.status}>{status}</div>
-        <div className={styles.boardRow}>
-          <Square value={this.state.squares[0]} index={0} onClick={this.handleClick} />
-          <Square value={this.state.squares[1]} index={1} onClick={this.handleClick} />
-          <Square value={this.state.squares[2]} index={2} onClick={this.handleClick} />
-        </div>
-        <div className={styles.boardRow}>
-          <Square value={this.state.squares[3]} index={3} onClick={this.handleClick} />
-          <Square value={this.state.squares[4]} index={4} onClick={this.handleClick} />
-          <Square value={this.state.squares[5]} index={5} onClick={this.handleClick} />
-        </div>
-        <div className={styles.boardRow}>
-          <Square value={this.state.squares[6]} index={6} onClick={this.handleClick} />
-          <Square value={this.state.squares[7]} index={7} onClick={this.handleClick} />
-          <Square value={this.state.squares[8]} index={8} onClick={this.handleClick} />
-        </div>
+function Board({ status, squares, handleClick }) {
+  return (
+    <div>
+      <div className={styles.status}>{status}</div>
+      <div className={styles.boardRow}>
+        <Square value={squares[0]} index={0} onClick={handleClick} />
+        <Square value={squares[1]} index={1} onClick={handleClick} />
+        <Square value={squares[2]} index={2} onClick={handleClick} />
       </div>
-    );
-  }
+      <div className={styles.boardRow}>
+        <Square value={squares[3]} index={3} onClick={handleClick} />
+        <Square value={squares[4]} index={4} onClick={handleClick} />
+        <Square value={squares[5]} index={5} onClick={handleClick} />
+      </div>
+      <div className={styles.boardRow}>
+        <Square value={squares[6]} index={6} onClick={handleClick} />
+        <Square value={squares[7]} index={7} onClick={handleClick} />
+        <Square value={squares[8]} index={8} onClick={handleClick} />
+      </div>
+    </div>
+  );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+Board.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+  squares: PropTypes.arrayOf(PropTypes.string).isRequired,
+  status: PropTypes.string.isRequired
+ };
+
+export default Board;
+
+export function calculateWinner(squares) {
+  const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
--- react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
 function Square({ value, index, onClick }) {
   return (
-    <button type="button"
-      className={styles.square}
-      onClick={() => onClick(index)}>
+    <button type="button" className={styles.square} onClick={() => onClick(index)}>
       {value}
     </button>
   );
 }
+
+Square.propTypes = {
+  index: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+ };
 
 export default Square;
