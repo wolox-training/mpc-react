56d8772056ff25547461e815f94f20819f08ac5a
Mariana <mariana.calmon@wolox.com.ar>
2019-07-16 09:59:25 -0300

    Component Matches.

--- react/tic-tac-toe/src/app/screens/Game/components/Matches/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Matches/index.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
+import MatchesService from '../../../../../services/MatchesService';
+
+import styles from './styles.module.scss';
+
+const Spinner = require('react-spinkit');
+
+const getWinnerClass = isWinner => (isWinner ? styles.winner : '');
+
 class Matches extends Component {
-  state = {};
+  state = {
+    data: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    const match = MatchesService.getMatches();
+    match.then(result => {
+      this.setState({ data: result.data, loading: false });
+    });
+  }
 
   render() {
-    return <div>HOLA</div>;
+    return (
+      <div>
+        {this.state.loading ? (
+          <Spinner name="three-bounce" color="#00ADEE" />
+        ) : (
+          <ol>
+            {this.state.data.map(item => (
+              <li key={item.id}>
+                <span className={getWinnerClass(item.winner === 'player_one')}>{item.player_one}</span> -{' '}
+                <span className={getWinnerClass(item.winner === 'player_two')}>{item.player_two}</span>
+              </li>
+            ))}
+          </ol>
+        )}
+      </div>
+    );
   }
 }
 
