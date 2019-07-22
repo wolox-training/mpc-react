02a35a56d5b8ddb3acc7f5fa7a3a21a53ce3f66b
Mariana <mariana.calmon@wolox.com.ar>
2019-07-22 09:53:18 -0300

    Change ...state for state.merge.

--- react/tic-tac-toe/src/redux/game/reducer.js
+++ react/tic-tac-toe/src/redux/game/reducer.js
@@ -10,23 +10,20 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case actions.ADD_MOVE:
-      return {
-        ...state,
+      return state.merge({
         history: state.history.concat([action.payload]),
         stepNumber: state.history.length,
         xIsNext: !state.xIsNext
-      };
+      });
     case actions.REMOVE_MOVE:
-      return {
-        ...state,
+      return state.merge({
         stepNumber: action.payload,
         xIsNext: action.payload % 2 === 0
-      };
+      });
     case actions.SET_WINNER:
-      return {
-        ...state,
+      return state.merge({
         winner: action.payload
-      };
+      });
     default:
       return state;
   }
