3413e8e335f30305adf863c0aad84d4286648a57
Mariana <mariana.calmon@wolox.com.ar>
2019-07-25 15:09:35 -0300

    Validation file changes.

--- react/tic-tac-toe/src/app/screens/Login/validation/index.js
+++ react/tic-tac-toe/src/app/screens/Login/validation/index.js
@@ -1,14 +1,21 @@
-export const validate = values => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Email inválido';
-  }
-  if (!values.password) {
-    errors.password = 'Required';
-  } else if (values.password.length < 8) {
-    errors.password = 'Password tiene que tener mínimo 8 caracteres';
-  }
-  return errors;
-};
+export const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
+
+export const minLength = value => (value && value.length < 8 ? 'Field must have a minimum of 8 characters' : undefined);
+
+// const validate = values => {
+//   const errors = {};
+//   if (!values.email) {
+//     errors.email = 'Required';
+//   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//     errors.email = 'Email inválido';
+//   }
+//   if (!values.password) {
+//     errors.password = 'Required';
+//   } else if (values.password.length < 8) {
+//     errors.password = 'Password tiene que tener mínimo 8 caracteres';
+//   }
+//   return errors;
+// };
+
+// export default validate;
