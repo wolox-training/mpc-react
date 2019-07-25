b955783d6d5787d2eeccbb69cc63356f9cd845bb
Mariana <mariana.calmon@wolox.com.ar>
2019-07-25 15:07:14 -0300

    Login component and its styles.

--- react/tic-tac-toe/src/app/screens/Login/components/FormLogin/index.js
+++ react/tic-tac-toe/src/app/screens/Login/components/FormLogin/index.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React from 'react';
+import { Field, reduxForm, Form } from 'redux-form';
 
-import { validate } from '../../validation';
+import { email, minLength } from '../../validation';
 
 import styles from './styles.module.scss';
 
-class FormLogin extends Component {
-  render() {
-    const { handleSubmit, meta } = this.props;
-    return (
-      <form onSubmit={handleSubmit} className={styles.formLogin}>
-        <div>
-          <label className={styles.formLabel}>Email</label>
-          <Field
-            name="name"
-            component="input"
-            type="text"
-            className={styles.formField}
-          />
-          {meta.error && meta.(
-            <div styles={{ color: 'red' }}>{meta.error}</div>
-          )}
-        </div>
-        <div>
-          <label className={styles.formLabel}>Password</label>
-          <Field
-            name="password"
-            component="input"
-            type="text"
-            className={styles.formField}
-          />
-        </div>
-        <button type="submit" className={styles.formButton}>
-          Submit
-        </button>
-      </form>
-    );
-  }
+function FormLogin({ handleSubmit }, meta) {
+  return (
+    <Form onSubmit={handleSubmit} className={styles.formLogin}>
+      <div>
+        <label className={styles.formLabel}>Email</label>
+        {meta && meta.error && meta.touched(<div styles={{color: 'red'}}>{meta.error}</div>)}
+        <Field name="email" component="input" type="text" className={styles.formField} validate={[email]} />
+      </div>
+      <div>
+        <label className={styles.formLabel}>Password</label>
+        {meta.error && meta.touched(<div className={styles.error}>{meta.error}</div>)}
+        <Field
+          name="password"
+          component="input"
+          type="password"
+          className={styles.formField}
+          validate={[minLength]}
+        />
+      </div>
+      <button type="submit" className={styles.formButton}>
+        Submit
+      </button>
+    </Form>
+  );
 }
 
-FormLogin = reduxForm({
-  form: 'login',
-  validate
+export default reduxForm({
+  form: 'login'
 })(FormLogin);
-
-export default FormLogin;
