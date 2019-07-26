319fc6a893d00dd5e05646cc5581ace495d2e6e0
Mariana <mariana.calmon@wolox.com.ar>
2019-07-26 13:55:49 -0300

    Validations done.

--- react/tic-tac-toe/src/app/screens/Login/validation/index.js
+++ react/tic-tac-toe/src/app/screens/Login/validation/index.js
@@ -1,4 +1,7 @@
-export const email = value =>
-  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
+export const required = value => (value ? undefined : 'Required field');
 
-export const minLength = value => (value && value.length < 8 ? 'Field must have a minimum of 8 characters' : undefined);
+export const email = value =>
+  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : undefined;
+
+export const minLength = value =>
+  value && value.length < 8 ? 'Field must have a minimum of 8 characters' : undefined;
