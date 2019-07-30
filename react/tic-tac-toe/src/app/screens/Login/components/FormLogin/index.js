5d33f3a59cd28acb369e36c002628af80d271da1
Mariana <mariana.calmon@wolox.com.ar>
2019-07-30 14:42:32 -0300

    Extract password to a constant.

--- react/tic-tac-toe/src/app/screens/Login/components/FormLogin/index.js
+++ react/tic-tac-toe/src/app/screens/Login/components/FormLogin/index.js
@@ -5,16 +5,17 @@ import CustomInput from '../Fields';
 import { required, email, minLength } from '../../validation';
 
 import styles from './styles.module.scss';
+import { password } from './constants';
 
 function FormLogin({ handleSubmit }) {
   return (
     <Form onSubmit={handleSubmit} className={styles.formLogin}>
       <Field name="email" component={CustomInput} type="text" label="Email" validate={[required, email]} />
       <Field
-        name="password"
+        name={password}
         component={CustomInput}
-        type="password"
-        label="Password"
+        type={password}
+        label={password.charAt(0).toUpperCase() + password.slice(1)}
         validate={[required, minLength]}
       />
       <button type="submit" className={styles.formButton}>
