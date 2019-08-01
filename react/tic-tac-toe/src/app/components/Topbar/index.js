3da663989201213b64357d34c373997f1d62be2c
Mariana <mariana.calmon@wolox.com.ar>
2019-08-01 10:28:02 -0300

    Add awesome font icons.

--- react/tic-tac-toe/src/app/components/Topbar/index.js
+++ react/tic-tac-toe/src/app/components/Topbar/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './styles.module.scss';
 
 function Topbar() {
   return (
     <nav className={styles.topbar}>
-      <ol>Puntuaciones</ol>
-      <ol>Logout</ol>
+      <li>
+        <a href="http://localhost:3001/login">Logout</a>
+        <FontAwesomeIcon icon="sign-out-alt" />
+      </li>
     </nav>
   );
 }
--- react/tic-tac-toe/src/app/index.js
+++ react/tic-tac-toe/src/app/index.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 import Topbar from './components/Topbar';
 import Game from './screens/Game';
 
 import '../scss/application.scss';
+
+library.add(faSignOutAlt);
 
 function App({ isLogged }) {
   return (
