a752bd48d35a4088f04315d7ecd31a241389f8c2
Mariana <mariana.calmon@wolox.com.ar>
2019-08-02 18:29:32 -0300

    Change login name to auth and complete actions and reduce logout.

---
+++ react/tic-tac-toe/src/redux/auth/reducer.js
@@ -0,0 +1,50 @@
+import Inmutable from 'seamless-immutable';
+
+import { actions } from './actions';
+
+const initialState = {
+  values: {},
+  isLogged: null,
+  error: null,
+  token: null
+};
+
+function reducer(state = Inmutable(initialState), action) {
+  switch (action.type) {
+    case actions.LOGIN:
+      return state.merge({
+        values: action.payload
+      });
+    case actions.LOGIN_SUCCESS:
+      return state.merge({
+        isLogged: true,
+        token: action.payload.token
+      });
+    case actions.LOGIN_FAILURE:
+      return state.merge({
+        error: action.payload
+      });
+    case actions.LOGOUT:
+      return state.merge({
+        values: {}
+      });
+    case actions.LOGOUT_SUCCESS:
+      return state.merge({
+        isLogged: null,
+        values: {},
+        token: null
+      });
+    case actions.LOGOUT_FAILURE:
+      return state.merge({
+        error: action.payload
+      });
+    case actions.AUTH_INIT:
+      return state.merge({
+        isLogged: action.payload
+      });
+    default:
+      return state;
+  }
+}
+
+export default reducer;
