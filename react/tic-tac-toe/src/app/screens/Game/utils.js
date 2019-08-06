6d8a2f2acb2f708831939bb2369b8b0d406040ed
Mariana <mariana.calmon@wolox.com.ar>
2019-08-06 10:54:11 -0300

    Remove the matches component from Game.

--- react/tic-tac-toe/src/app/screens/Game/utils.js
+++ react/tic-tac-toe/src/app/screens/Game/utils.js
@@ -1,4 +1,4 @@
-import styles from '../Game/components/Matches/styles.module.scss';
+import styles from '../PointHistory/Matches/styles.module.scss';
 
 export function calculateWinner(squares) {
   const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
