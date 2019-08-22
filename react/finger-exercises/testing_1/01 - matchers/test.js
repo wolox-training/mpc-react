290e03fc6d79536e6f96709386448a279ecbc19d
Mariana <mariana.calmon@wolox.com.ar>
2019-08-22 11:17:16 -0300

    Test 1.

--- react/finger-exercises/testing_1/01 - matchers/test.js
+++ react/finger-exercises/testing_1/01 - matchers/test.js
@@ -1,8 +1,8 @@
 import pow from '.';
 
 describe('01 - matchers', () => {
-  xit('pow returns the power based on two numeric arguments', () => {
-    pow();
+  it('pow returns the power based on two numeric arguments', () => {
+    expect(pow(3, 2)).toBe(9);
   });
   xit('pow returns undefined if there is no arguments', () => {
   });
