2c27167abc1a8e68bba41eb1ce5136959d0db5fd
Mariana <mariana.calmon@wolox.com.ar>
2019-08-22 17:14:40 -0300

    Test async 3.

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -8,7 +8,9 @@ describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
     expect(getData(true)).resolves.toBe();
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    await getData(false)
+      .catch(data => expect(data).toBeTruthy());
   });
   xit('getData throws error if false is sent as argument (avoid async/await)', () => {
   });
