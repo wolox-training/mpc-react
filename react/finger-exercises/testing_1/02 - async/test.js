c9421a30e6e9e8e1e03a7100dc9da7d22c431888
Mariana <mariana.calmon@wolox.com.ar>
2019-08-23 14:15:37 -0300

    Refactoring.

--- react/finger-exercises/testing_1/02 - async/test.js
+++ react/finger-exercises/testing_1/02 - async/test.js
@@ -2,14 +2,11 @@ import getData from '.';
 
 describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (use async/await)', async () => {
-    expect(await getData(true)).toBeDefined();
+    await expect(getData(true)).toBeDefined();
   });
-  it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
-    expect(getData(true)).resolves.toBe();
+  it('getData resolves the data if true is sent as argument (avoid async/await)', () => expect(getData(true)).resolves.toBeDefined());
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    await expect(getData(false)).rejects.toThrow('error');
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
-  });
-  it('getData throws error if false is sent as argument (avoid async/await)', () => {
-    expect(getData(false)).rejects.toThrow('error');
-  });
+  it('getData throws error if false is sent as argument (avoid async/await)', () => expect(getData(false)).rejects.toThrow('error'));
 });
